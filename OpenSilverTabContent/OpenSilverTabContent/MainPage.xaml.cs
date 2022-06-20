672e224b0f5abc2fdd20db00d08e2a74d3df2d31
Iakov Lilo <jlilo@hwdtech.com>
2022-06-20 16:01:48 +0600

    Add project files.

---
+++ OpenSilverTabContent/OpenSilverTabContent/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverTabContent
+{
+    public partial class MainPage : Page
+    {
+        private readonly TabItem _tabItem1;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            _tabItem1 = new TabItem
+            {
+                Header = "Header",
+                Content = "Initial Content"
+            };
+
+            MyTabControl.Items.Add(_tabItem1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _tabItem1.Content = new TextBlock { Text = "Changed Content at " + DateTime.Now.ToString("s") };
+        }
+    }
+}
